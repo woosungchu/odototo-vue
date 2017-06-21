1403321c465bb311269ed6871c1654788521b3a1
woosungchu <woosungchu@gmail.com>
2017-06-21 17:26:29 +0900

    add Tab to Words service

--- src/components/words/Words.vue
+++ src/components/words/Words.vue
@@ -1,17 +1,50 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-  </div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">단어 맞추기</h1>
+      </div>
+    </section>
+
+	<div class="tabs">
+	  <ul>
+	    <li v-on:click="updateSelected(1)" v-bind:class="{ 'is-active': selectedTab == 1 }"><a>기본</a></li>
+	    <li v-on:click="updateSelected(2)" v-bind:class="{ 'is-active': selectedTab == 2 }"><a>줄긋기</a></li>
+	  </ul>
+	</div>
+	
+	<!-- tab-contents -->
+	<div class="tab-content" v-show="selectedTab == 1">
+	  <words-scramble></words-scramble>
+	</div>
+	<div class="tab-content" v-show="selectedTab == 2">
+	  <words-matchup></words-matchup>
+	</div>
+	
+  </div>	
 </template>
 
 <script>
+import WordsScramble from './WordsScramble'
+import WordsMatchup from './WordsMatchup'
+
 export default {
   name: 'words',
+  components: {
+  	WordsScramble,
+  	WordsMatchup
+  },
   data () {
     return {
-      msg: 'Try Words!'
+      selectedTab: 1
+    }
+  },
+  methods: {
+    updateSelected: function(tab) {
+      this.selectedTab = tab;
     }
   }
+  
 }
 </script>
 
---
+++ src/components/words/WordsMatchup.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  	<img src="//placehold.it/150x100?text=Matchup" alt="">
+    <p>Matchup Tab</p>
+  </div>	
+</template>
+
+<script>
+export default {
+  name: 'words-matchup'
+}
+</script>
