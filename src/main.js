b70fdfeb584d91ab5e8a031318e7f330130bdc63
woosungchu <woosungchu@gmail.com>
2017-06-21 13:58:56 +0900

    remove sweetalert.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'sweetalert/dist/sweetalert.css'
+
 Vue.config.productionTip = false
+
+
 
 new Vue({
   el: '#app',
