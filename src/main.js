ce01b98f00fc91bef4b24a28621120dcd2c0d195
woosungchu <woosungchu@gmail.com>
2017-06-21 14:19:10 +0900

    add Nav with bulma

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'bulma/css/bulma.css'
 import 'sweetalert/dist/sweetalert.css'
 
 Vue.config.productionTip = false
