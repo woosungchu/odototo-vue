ba8edfccdcd1582f2a515b11d96875165781d04f
woosungchu <woosungchu@gmail.com>
2017-06-29 15:44:32 +0900

    add Google Analytics

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import router from './router'
 import 'bulma/css/bulma.css'
 import 'sweetalert/dist/sweetalert.css'
 
+import VueAnalytics from 'vue-analytics'
+
 Vue.config.productionTip = false
 
-
+if(process.env.NODE_ENV === 'production'){
+	Vue.use(VueAnalytics, {
+	  id: 'UA-83009418-2',
+	  router
+	})
+}
 
 new Vue({
   el: '#app',
