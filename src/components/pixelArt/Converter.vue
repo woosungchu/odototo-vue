cabb5093991ccc015a0ebfa32ba087cb6a74515f
woosungchu <woosungchu@gmail.com>
2017-06-29 18:00:28 +0900

    canvasing

--- src/components/pixelArt/Converter.vue
+++ src/components/pixelArt/Converter.vue
@@ -10,9 +10,7 @@
         </figure>
       </div>
       <div class="column">
-        <figure class="image is-4by3">
-            <img class="img-responsive" src="http://placehold.it/700x400?text=ART" alt="TEST">
-        </figure>
+        <canvas id="pixel-art-canvas"></canvas>
       </div>
     </div>
 
@@ -33,16 +31,46 @@ export default {
     displayImage: function(e){
       let fileInput = e.target,
           image = this.$el.querySelector('#upload-image'),
+          canvas = this.$el.querySelector('#pixel-art-canvas'),
           fileReader = new FileReader();
+          
+          
 
       fileReader.onload = function(e2){
         image.src = e2.target.result;
+        
+        canvas.width = image.width;
+        canvas.height = image.height;
       }
 
       fileReader.readAsDataURL(fileInput.files[0]);
     },//displayImage
     convert(){
-      alert('convert');
+      let canvas = this.$el.querySelector('#pixel-art-canvas'),
+      	  image = this.$el.querySelector('#upload-image'),
+      	  ctx = canvas.getContext('2d'),
+          degree = 120,
+          img = new Image,
+          imgd,pix;
+
+      img.src = image.src;
+      ctx.drawImage(img,0,0);
+
+      imgd = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      pix = imgd.data;
+
+      for (let i = 0, n = pix.length; i <n; i += 4) {
+          let r = pix[i],
+              g = pix[i+1],
+              b = pix[i+2],
+              L = r * 299/1000 + g * 587/1000 + b * 114/1000;
+
+          if(L > degree){
+            pix[i] = pix[i+1] = pix[i+2] = 255; //white
+          }
+      }
+      ctx.putImageData(imgd,0,0);
+      
     }
 
   }
