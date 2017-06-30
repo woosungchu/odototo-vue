f1581d74ecb673f35686c0822e5563198c930cf2
woosungchu <woosungchu@gmail.com>
2017-07-01 00:57:40 +0900

    keep converting...

--- src/components/pixelArt/Converter.vue
+++ src/components/pixelArt/Converter.vue
@@ -33,12 +33,12 @@ export default {
           image = this.$el.querySelector('#upload-image'),
           canvas = this.$el.querySelector('#pixel-art-canvas'),
           fileReader = new FileReader();
-          
-          
+
+
 
       fileReader.onload = function(e2){
         image.src = e2.target.result;
-        
+
         canvas.width = image.width;
         canvas.height = image.height;
       }
@@ -52,66 +52,48 @@ export default {
           degree = 10,
           img = new Image,
           tiles = Math.pow(degree,2),
-          ctxArr = [],
           eachWidth,eachHeight;
 
       img.src = image.src;
       ctx.drawImage(img,0,0);
-      
+
       eachWidth= canvas.width/degree;
       eachHeight= canvas.height/degree;
-      
+
       for(let k = 0; k < tiles; k++) {
       	let imgd,x,y;
-      	
-      	x = (k % degree) * eachWidth; 
+
+      	x = (k % degree) * eachWidth;
       	y = (k / degree) * eachHeight;
       	imgd = ctx.getImageData(x, y, eachWidth, eachHeight);
-      	ctxArr.push(imgd.data);
-      }
-      
-      for (let j = 0; j < ctxArr.length; j++) {
-      	  let pix = ctxArr[j],
-      	  	  rgb = {r:0,g:0,b:0},
-      	  	  blockSize = 5,
+
+        let pix = imgd.data,
+    	  	  rgb = {r:0,g:0,b:0},
+    	  	  blockSize = 5,
 	      	  count = 0,
 	      	  i = -4;
-      	
-          while ( (i += blockSize * 4) < pix.length ) {
-		      ++count;
-		      rgb.r += pix[i];
-		      rgb.g += pix[i+1];
-		      rgb.b += pix[i+2];
-		  }
-	      
+
+        while ( (i += blockSize * 4) < pix.length ) {
+  		      ++count;
+  		      rgb.r += pix[i];
+  		      rgb.g += pix[i+1];
+  		      rgb.b += pix[i+2];
+  		  }
+
 	      rgb.r = ~~(rgb.r/count);
 	      rgb.g = ~~(rgb.g/count);
 	      rgb.b = ~~(rgb.b/count);
-	      
-	      console.log(rgb);
-      }
+        i = -4;
 
-	  /*
-	  https://stackoverflow.com/questions/2541481/get-average-color-of-image-via-javascript
-	  
-      imgd = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      pix = imgd.data;
+        while ( (i += blockSize * 4) < pix.length ) {
+  		      pix[i] = rgb.r;
+  		      pix[i+1] = rgb.g;
+  		      pix[i+2] = rgb.b;
+  		  }
 
-	  
-      for (let i = 0, n = pix.length; i <n; i += 4) {
-          let r = pix[i],
-              g = pix[i+1],
-              b = pix[i+2],
-              L = r * 299/1000 + g * 587/1000 + b * 114/1000;
+        ctx.putImageData(imgd, 0,0 ,x, y, eachWidth, eachHeight);
+      }//end for
 
-          if(L > degree){
-            pix[i] = pix[i+1] = pix[i+2] = 255; //white
-          }
-      }
-      
-      ctx.putImageData(imgd,0,0);
-      */
-      
     }
 
   }
